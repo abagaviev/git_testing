ac9035f0e884f99cbdb2796f16036ecdc00261aa
Azat <public@mail.com>
2022-04-13 00:45:52 +0300

    learning DB

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-print('testing how it works')
-print(1 + 2 + 3)
+import sqlite3 as sq
+
+with sq.connect("saper.db") as con:
+    cur = con.cursor()
+    cur.execute('''CREATE TABLE IF NOT EXISTS users (
+        name INTEGER,
+        sex INTEGER, 
+        old INTEGER,
+        score INTEGER
+        )''')
