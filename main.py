a86223b3015e1ddf5e3e33b102440c94ba0d7e23
Azat <public@mail.com>
2022-04-15 15:58:22 +0300

    DB testing

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import sqlite3 as sq
 
 with sq.connect("saper.db") as con:
     cur = con.cursor()
-    cur.execute('''CREATE TABLE IF NOT EXISTS users (
-        name INTEGER,
-        sex INTEGER, 
-        old INTEGER,
-        score INTEGER
+    cur.execute('''CREATE TABLE IF NOT EXISTS books (
+        book_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        date INTEGER, 
+        author TEXT
         )''')
